01c137853a157021ecbafbb1e8b2a3f741a7ad71
Yash Chaudhari <chaudhariyash10@gmail.com>
2020-09-04 11:31:15 +0530

    Added Carousel

--- JS/main.js
+++ JS/main.js
@@ -4,14 +4,25 @@ $(document).ready(function () {
   $btns.click(function (e) {
     $(".project-area .button-group button").removeClass("active");
     e.target.classList.add("active");
-
+    //Isotope Filter Code=========================================================================
     let selector = $(e.target).attr("data-filter");
     $(".project-area .grid").isotope({
       filter: selector,
     });
+      //===========================================================================================
 
     return false;
   });
     
     $(".project-area .button-group #btn1").trigger("click");
+
+  //Magnific PopUp Code=============================================================================
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    })
+    //===============================================================================================
 });
